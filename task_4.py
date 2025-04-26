9003318b8eab43a14a664339f12a8be9370455b3
sveta <sveta@steblova.ru>
2025-04-26 20:19:17 +0500

    Корректировка задания 4

--- task_4.py
+++ task_4.py
@@ -23,20 +23,3 @@ class EmployeeSalary:
 
     def salary(self):
         return self.hours * self.hourly_payment
-
-#Тесты
-# создать экземпляр, не имея значения hours   
-employee1 = EmployeeSalary.get_hours('Sveta', 2, 'sveta@mail.ru')
-print(f'Для первого сотрудника установлено часов рабочего времени: {employee1.hours}') #40
-
-#Рассчет ЗП
-print(f'Зароботная плата первого сотрудника: {employee1.salary()}')
-
-#создать экземпляр, не имея email
-employee2 = EmployeeSalary.get_email('Maria', 48, 1)
-print(f'Для второго сотрудника установлена почта: {employee2.email}') #Maria@email.com
-
-# разные значения hourly_payment
-print(f'Ставка сотрудника до изменения: {employee2.hourly_payment}') #400 - экземпляр до замены ставки
-EmployeeSalary.set_hourly_payment(100)
-print(f'Ставка сотрудника после изменения: {employee2.hourly_payment}') #100 - экземпляр после замены ставки
